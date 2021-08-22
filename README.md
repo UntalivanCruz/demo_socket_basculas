fdc5ddfe39cdafbea2c23a8092cf1d64eb6094bd
Ivan Cruz <ivancruzhn@gmail.com>
2021-08-21 22:39:37 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Demo Socket Basculas
+
+_Este demo permite simular una comunicacion RS232 con basculas y permitir una conexion con multiples clientes por medio de socket_
+
+### Instalación 🔧
+
+_Instalar las dependendecias necesarias_
+
+```
+npm install
+```
+
+## Despliegue 📦
+
+_Heroku_
+
+## Construido con 🛠️
+
+* [Express](https://www.npmjs.com/package/express) - HTTP
+* [Socket.io](https://www.npmjs.com/package/socket.io) - Socket
+
+## Autores ✒️
+* **Ivan Cruz** - *IT FALCON* - [talivan00220](https://github.com/talivan00220/)
+
+## Licencia 📄
+
+Este proyecto está bajo la Licencia ISC
---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 80);
+const server = app.listen(app.get('port'),()=>{
+    console.log('Server on port', app.get('port'))
+})
+
+const SocketIO = require('socket.io');
+const io = SocketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('nueva conexion', socket.id);
+    sendData(socket);
+});
+
+function sendData(socket){
+    socket.emit('bascula1', 220+Math.floor(Math.random() * 20));
+    socket.emit('bascula2', 200+Math.floor(Math.random() * 20));
+    socket.emit('bascula3', 230+Math.floor(Math.random() * 20));
+    socket.emit('bascula4', 100+Math.floor(Math.random() * 20));
+    socket.emit('bascula5', 150+Math.floor(Math.random() * 20));
+
+    setTimeout(() => {
+        sendData(socket);
+    }, 500)
+}
